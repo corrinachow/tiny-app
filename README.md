6166e0b97dc2ef973ef3e4e9bb37f1e8cdef7155
corrinachow <corrina.yy.chow@gmail.com>
2018-04-20 23:26:47 -0400

    Add coments and update README.md

--- README.md
+++ README.md
@@ -46,83 +46,272 @@ so that I can learn what content they like.
 ## Display Requirements
 
 <ul>
-  <li>Site Header:
-    <ul>
-      <li>if a user is logged in, the header shows:
-        <ul>
-          <li>✅ the user's email</li>
-          <li>✅ a logout button which makes a POST request to <code>/logout</code></li>
-        </ul>
-      </li>
-      <li>if a user is not logged in, the header shows:
-        <ul>
-          <li> ✅ a link to the login page (<code>/login</code>)</li>
-          <li> ✅ a link to the registration page (<code>/register</code>)</li>
-        </ul>
-      </li>
-    </ul>
-  </li>
+<li>Site Header:
+<ul>
+<li>if a user is logged in, the header shows:
+<ul>
+<li>✅ the user's email</li>
+<li>✅ a logout button which makes a POST request to <code>/logout</code></li>
+</ul>
+</li>
+<li>if a user is not logged in, the header shows:
+<ul>
+<li> ✅ a link to the login page (<code>/login</code>)</li>
+<li> ✅ a link to the registration page (<code>/register</code>)</li>
+</ul>
+</li>
+</ul>
+</li>
 </ul>
 
 ## Behaviour Requirements
 
+
 <ul>
-  <li><b><code>GET /</code></b>
-    <ul>
-      <li>if user is logged in:
-        <ul>
-          <li>(<b>Minor</b>) ✅ redirect to <code>/urls</code></li>
-        </ul>
-      </li>
-      <li>if user is not logged in:
-        <ul>
-          <li>(<b>Minor</b>) ✅ redirect to <code>/login</code>
-        </ul>
-      </li>
-    </ul>
-  </li>
-  <li><b><code>GET /urls</li></code></b>
-    <ul>
-      <li>if user is logged in:
-        <ul>
-          <li>returns HTML with:
-            <ul>
-              <li>✅ the site header (see Display Requirements above)</li>
-              <li>✅ a list (or table) of URLs the user has created, each list item containing:
-                <ul>
-                  <li> ✅ a short URL</li>
-                  <li> ✅ the short URL's matching long URL</li>
-                  <li> ✅ an edit button which makes a GET request to <code>/urls/:id</code></li>
-                  <li> ✅ a delete button which makes a POST request to <code>/urls/:id/delete</code></li>
-                  <li>(<b>Stretch</b>) ✅ the date the short URL was created</li>
-                  <li>(<b>Stretch</b>) ✅ the number of times the short URL was visited</li>
-                  <li>(<b>Stretch</b>) ✅ the number  of unique visits for the short URL</li>
-                </ul>
-              </li>
-              <li>(<b>Minor</b>) ✅ a link to "Create a New Short Link" which makes a GET request to <code>/urls/new</code></li>
-            </ul>
-            <li>if user is not logged in:
-              <ul>
-                <li> ✅ returns HTML with a relevant error message</li>
-          </li>
-        </ul>
-        <li><b><code>GET /urls/new</code></b>
-          <ul>
-            <li>if user is logged in:
-              <ul>
-                <li> ✅ returns HTML with:</li>
-                <li> ✅ the site header (see Display Requirements above)</li>
-                <li>a form which contains:
-                  <ul>
-                    <li> ✅ a text input field for the original (long) <code>URL</code></li>
-                    <li> ✅ a submit button which makes a POST request to <code>/url</code></li>
-                  </ul>
-                </li>
-              </ul>
-            <li> ✅ if user is not logged in:
-              <ul>
-                <li> ✅ redirects to the <code>/login</li> page</li>
-      </li>
-    </ul>
-  </li>
+<li><p><code>GET /</code></p>
+
+<ul>
+<li>if user is logged in:
+
+<ul>
+<li> ✅ (Minor) redirect to <code>/urls</code></li>
+</ul></li>
+<li>if user is not logged in:
+
+<ul>
+<li>✅ (Minor) redirect to <code>/login</code></li>
+</ul></li>
+</ul></li>
+<li><p><code>GET /urls</code></p>
+
+<ul>
+<li>if user is logged in:
+
+<ul>
+<li>returns HTML with:</li>
+<li> ✅ the site header (see Display Requirements above)</li>
+<li> ✅ a list (or table) of URLs the user has created, each list item containing:
+
+<ul>
+<li> ✅ a short URL</li>
+<li> ✅ the short URL's matching long URL</li>
+<li> ✅ an edit button which makes a GET request to <code>/urls/:id</code></li>
+<li> ✅ a delete button which makes a POST request to <code>/urls/:id/delete</code></li>
+<li> ✅ (Stretch) the date the short URL was created</li>
+<li> ✅ (Stretch) the number of times the short URL was visited</li>
+<li> ✅ (Stretch) the number number of unique visits for the short URL</li>
+</ul></li>
+<li> ✅ (Minor) a link to "Create a New Short Link" which makes a GET request to <code>/urls/new</code></li>
+</ul></li>
+<li>if user is not logged in:
+
+<ul>
+<li> ✅ returns HTML with a relevant error message</li>
+</ul></li>
+</ul></li>
+<li><p><code>GET /urls/new</code></p>
+
+<ul>
+<li>if user is logged in:
+
+<ul>
+<li>returns HTML with:</li>
+<li> ✅ the site header (see Display Requirements above)</li>
+<li>a form which contains:
+
+<ul>
+<li> ✅ a text input field for the original (long) URL</li>
+<li> ✅ a submit button which makes a POST request to <code>/urls</code></li>
+</ul></li>
+</ul></li>
+<li>if user is not logged in:
+
+<ul>
+<li>redirects to the <code>/login</code> page</li>
+</ul></li>
+</ul></li>
+<li><p><code>GET /urls/:id</code></p>
+
+<ul>
+<li>if user is logged in and owns the URL for the given ID:
+
+<ul>
+<li>returns HTML with:</li>
+<li> ✅ the site header (see Display Requirements above)</li>
+<li> ✅ the short URL (for the given ID)</li>
+<li>a form which contains:
+
+<ul>
+<li> ✅ the corresponding long URL</li>
+<li> ✅ an update button which makes a POST request to <code>/urls/:id</code></li>
+</ul></li>
+<li> ✅ (Stretch) the date the short URL was created</li>
+<li> ✅ (Stretch) the number of times the short URL was visited</li>
+<li> ✅ (Stretch) the number of unique visits for the short URL</li>
+</ul></li>
+<li>if a URL for the given ID does not exist:
+
+<ul>
+<li> ✅ (Minor) returns HTML with a relevant error message</li>
+</ul></li>
+<li>if user is not logged in:
+
+<ul>
+<li> ✅ returns HTML with a relevant error message</li>
+</ul></li>
+<li>if user is logged it but does not own the URL with the given ID:
+
+<ul>
+<li> ✅ returns HTML with a relevant error message</li>
+</ul></li>
+</ul></li>
+<li><p><code>GET /u/:id</code></p>
+
+<ul>
+<li>if URL for the given ID exists:
+
+<ul>
+<li> ✅ redirects to the corresponding long URL</li>
+</ul></li>
+<li>if URL for the given ID does not exist:
+
+<ul>
+<li> ✅ (Minor) returns HTML with a relevant error message</li>
+</ul></li>
+</ul></li>
+<li><p><code>POST /urls</code></p>
+
+<ul>
+<li>if user is logged in:
+
+<ul>
+<li> ✅ generates a short URL, saves it, and associates it with the user</li>
+<li> ✅ redirects to <code>/urls/:id</code>, where <code>:id</code> matches the ID of the newly saved URL</li>
+</ul></li>
+<li>if user is not logged in:
+
+<ul>
+<li> ✅ (Minor) returns HTML with a relevant error message</li>
+</ul></li>
+</ul></li>
+<li><p><code>POST /urls/:id</code></p>
+
+<ul>
+<li>if user is logged in and owns the URL for the given ID:
+
+<ul>
+<li> ✅ updates the URL</li>
+<li> ✅ redirects to <code>/urls</code></li>
+</ul></li>
+<li>if user is not logged in:
+
+<ul>
+<li> ✅ (Minor) returns HTML with a relevant error message</li>
+</ul></li>
+<li>if user is logged it but does not own the URL for the given ID:
+
+<ul>
+<li> ✅ (Minor) returns HTML with a relevant error message</li>
+</ul></li>
+<li><code>POST /urls/:id/delete</code></li>
+<li>if user is logged in and owns the URL for the given ID:
+
+<ul>
+<li> ✅ deletes the URL</li>
+<li> ✅ redirects to <code>/urls</code></li>
+</ul></li>
+<li>if user is not logged in:
+
+<ul>
+<li> ✅ (Minor) returns HTML with a relevant error message</li>
+</ul></li>
+<li>if user is logged it but does not own the URL for the given ID:
+
+<ul>
+<li> ✅ (Minor) returns HTML with a relevant error message</li>
+</ul></li>
+</ul></li>
+<li><p><code>GET /login</code></p>
+
+<ul>
+<li>if user is logged in:
+
+<ul>
+<li> ✅ (Minor) redirects to <code>/urls</code></li>
+</ul></li>
+<li>if user is not logged in:
+
+<ul>
+<li>returns HTML with:</li>
+<li>a form which contains:
+
+<ul>
+<li> ✅ input fields for email and password</li>
+<li> ✅ submit button that makes a POST request to <code>/login</code></li>
+</ul></li>
+</ul></li>
+</ul></li>
+<li><p><code>GET /register</code></p>
+
+<ul>
+<li>if user is logged in:
+
+<ul>
+<li> ✅ (Minor) redirects to <code>/urls</code></li>
+</ul></li>
+<li>if user is not logged in:
+
+<ul>
+<li>returns HTML with:</li>
+<li>a form which contains:
+
+<ul>
+<li> ✅ input fields for email and password</li>
+<li> ✅ a register button that makes a POST request to <code>/register</code></li>
+</ul></li>
+</ul></li>
+</ul></li>
+<li><p><code>POST /login</code></p>
+
+<ul>
+<li>if email and password params match an existing user:
+
+<ul>
+<li> ✅ sets a cookie</li>
+<li> ✅ redirects to <code>/urls</code></li>
+</ul></li>
+<li>if email and password params don't match an existing user:
+
+<ul>
+<li> ✅ returns HTML with a relevant error message</li>
+</ul></li>
+</ul></li>
+<li><p><code>POST /register</code></p>
+
+<ul>
+<li>if email or password are empty:
+
+<ul>
+<li> ✅ returns HTML with a relevant error message</li>
+</ul></li>
+<li>if email already exists:
+
+<ul>
+<li> ✅ returns HTML with a relevant error message</li>
+</ul></li>
+<li>otherwise:
+
+<ul>
+<li> ✅ creates a new user</li>
+<li> ✅ encrypts the new user's password with <code>bcrypt</code></li>
+<li> ✅ sets a cookie</li>
+<li> ✅ redirects to <code>/urls</code></li>
+</ul></li>
+</ul></li>
+<li><p><code>POST /logout</code></p>
+
+<ul>
+<li> ✅ deletes cookie</li>
+<li> ✅ redirects to <code>/urls</code></li>
+</ul></li>
 </ul>
--- server.js
+++ server.js
@@ -2,25 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
-
 const app = express();
 const PORT = 8080;
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: 'session',
   keys: ['dfshjkaf90dsafjk']
 }));
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
 const urlDatabase = {
-  'b2xVn2': { userID:'userRandomID', url:'http://www.lighthouselabs.ca', date: 'Thu Apr 19 2018 21:32:50 GMT-0400 (EDT)', ipOfVisitors: ['12.345.67.890', '12.345.67.890'] },
-  '9sm5xK': { userID:'user2RandomID', url:'http://www.google.com', date: 'Thu Apr 19 2018 21:32:50 GMT-0400 (EDT)', ipOfVisitors: ['12.345.67.890']},
-  '5x6rvqp': { userID:'user3RandomID', url:'https://wikipedia.org', date: 'Thu Apr 19 2018 21:32:50 GMT-0400 (EDT)', ipOfVisitors: ['01.234.56.789', '12.345.67.890', '12.345.67.890']}
+  'b2xVn2': {
+    userID:'userRandomID',
+    url:'http://www.lighthouselabs.ca',
+    date: 'Thu Apr 19 2018 21:32:50 GMT-0400 (EDT)',
+    ipOfVisitors: ['12.345.67.890', '12.345.67.890']
+  },
+  '9sm5xK': {
+    userID:'user2RandomID',
+    url:'http://www.google.com',
+    date: 'Thu Apr 19 2018 21:32:50 GMT-0400 (EDT)',
+    ipOfVisitors: ['12.345.67.890']
+  },
+  '5x6rvqp': {
+    userID:'user3RandomID',
+    url:'https://wikipedia.org',
+    date: 'Thu Apr 19 2018 21:32:50 GMT-0400 (EDT)',
+    ipOfVisitors: ['01.234.56.789', '12.345.67.890', '12.345.67.890']
+  }
 };
 
 const users = {
@@ -41,8 +52,7 @@ const users = {
   }
 };
 
-/* REGISTRATION */
-
+// Retrieves user object from email
 function getUser(email) {
   const currentUsers = Object.keys(users);
   for (const user_id of currentUsers) {
@@ -51,137 +61,245 @@ function getUser(email) {
     }
   }
   return false;
-}
+};
 
+// Retrieves short URLs associate with user ID
 function urlsForUser(id) {
   const urls = {};
   const shortURLs = Object.keys(urlDatabase);
   for (const shortURL of shortURLs) {
     if (urlDatabase[shortURL].userID === id) {
       urls[shortURL] = urlDatabase[shortURL];
-    };
-  };
+    }
+  }
   return urls;
-}
+};
 
+
+// Gets unique values from array
 function getUnique(value, index, element) {
   return element.indexOf(value) === index;
-}
-
-app.get('/register', (req, res) => {
-  console.log(req.session.user_id)
-  let templateVars = { urls: urlDatabase,
-    user: users[req.session.user_id]
-  };
-  res.render('urls_register');
-});
+};
 
 function validateEmail(email) {
   const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   return re.test(email.toLowerCase());
-}
+};
 
 function validateWebsite(longURL) {
   const re = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/i;
   return re.test(longURL.toLowerCase());
-}
+};
+
+function generateRandomStr() {
+  return Math.random().toString(36).substring(6);
+};
+
+/**
+ * ------------------------------------------------------------------------
+ * APP JSON DATA
+ * ------------------------------------------------------------------------
+ */
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+/**
+ * ------------------------------------------------------------------------
+ * USER REGISTRATION
+ * ------------------------------------------------------------------------
+ */
+
+// If user is logged in, redirects to /urls, otherwise renders registration page
+app.get('/register', (req, res) => {
+  const { user_id } = req.session
+  if (user_id) {
+    return res.redirect('/urls');
+  };
+  let templateVars = { urls: urlDatabase,
+    user: users[user_id]
+  };
+  return res.render('urls_register');
+});
+
+/**
+* Makes POST to /register to create new account if user enters valid email and password
+*
+* Adds new user information to users database, hash password, and sets cookie
+*
+* Redirects user to /urls upon successful account creation
+*/
 
 app.post('/register', (req, res) => {
   const { email, password } = req.body;
+
+  // Checks for email and password
   if (!email || !password) {
-    return res.status(400).send('Invalid email or password');
+    return res.status(400).send('<h1>Invalid email or password</h1>');
+
+    // Checks if email is already used in database
   } else if (getUser(email)) {
-    return res.status(400).send('Email already in use');
+    return res.status(400).send('<h1>Email already in use</h1>');
+
+    // Checks for a valid email
   } else if(!validateEmail(email)) {
-    return res.status(400).send('Please enter a valid email');
+    return res.status(400).send('<h1>Please enter a valid email</h1>');
+
+    // Populates new user object
   } else {
     const newUser = {
       id: generateRandomStr(),
       email: email,
       password: bcrypt.hashSync(password, 10),
-    };
+    }
     users[newUser['id']] = newUser;
-    req.session.user_id = newUser.id
+
+    // Sets cookie
+    req.session.user_id = newUser.id;
     let templateVars = { urls: urlDatabase,
     user: users[req.session.user_id] };
-    res.redirect('/urls');
+    return res.redirect('/urls');
   };
 });
 
+
+// Clears cookies on logout
 app.post('/logout', (req, res) => {
   delete req.session.user_id;
-  res.redirect('/urls');
+  return res.redirect('/urls');
 });
 
-/* HOME PAGE */
+/**
+ * ------------------------------------------------------------------------
+ * HOME PAGE
+ * ------------------------------------------------------------------------
+ */
 
 app.get('/' , (req, res) => {
+  // Redirects to /urls if user is already logged in, otherwise redirects to login page
   if (req.session.user_id) {
-    res.redirect('/urls');
+    return res.redirect('/urls');
   } else {
-    res.redirect('/login');
+    return res.redirect('/login');
   }
 });
 
-/* LOGIN FORM */
+/**
+ * ------------------------------------------------------------------------
+ * LOGIN FORM
+ * ------------------------------------------------------------------------
+ */
 
 app.get('/login' , (req, res) => {
-  res.render('login_form')
+  // Checks for user_id from cookies and redirects to /urls
+  if (req.session.user_id) {
+    return res.redirect('/urls');
+  }
+  return res.render('login_form');
 });
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
-  //user returns either user Obj or false;
+
+  // Retrieves user info object from email, returns false if user does not exist
   const user = getUser(email);
+
+  // Redirects to /urls upon successful authentication
   if (user && bcrypt.compareSync(password, user.password)) {
     req.session.user_id = user.id;
-    res.redirect('/urls')
+    return res.redirect('/urls')
   } else {
-    res.status(403).send('403: Email or password invalid');
+    return res.status(403).send('<h1>403: Email or password invalid</h1><a href="/login">Go back</a>');
   }
 });
 
-/* POST LOGIN */
+/**
+ * ------------------------------------------------------------------------
+ * USER CONTROLS
+ * ------------------------------------------------------------------------
+ */
+
+
+/**
+* Displays all short URLs create by user along with:
+* - creation date
+* - long URL
+* - times visited
+* - unique visits
+*
+* Authorised users can delete/edit their short URLs
+*
+* Users can create new short URLs
+*/
 
 app.get('/urls', (req, res) => {
   let templateVars = {
     user: users[req.session.user_id]
   };
+
+  // Retrieves all short URLs associated with logged in user
   if (req.session.user_id) {
     templateVars.urls = urlsForUser(req.session.user_id);
-  }
-  console.log(templateVars)
-  res.render('urls_index', templateVars);
+  };
+  return res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
+
+  // Redirects non-logged-in users to login page
   if (!req.session.user_id) {
     res.redirect('/login');
   } else {
-    let templateVars = { urls: urlDatabase,
+    let templateVars = {
+      urls: urlDatabase,
       user: users[req.session.user_id]
     };
-    res.render('urls_new', templateVars);
-  };
+    return res.render('urls_new', templateVars);
+  }
 });
 
-/* MAKES NEW URL */
+/**
+ * ------------------------------------------------------------------------
+ * NEW SHORT URL GENERATOR
+ * ------------------------------------------------------------------------
+ */
 
 app.post('/urls', (req, res) => {
   const { longURL } = req.body;
+
   if (!validateWebsite(longURL)) {
-    return res.status(400).send('Please enter a valid URL')
+    return res.status(400).send('Please enter a valid URL');
   };
   const shortURL = generateRandomStr();
+
+  // Adds new shortURL to database
   urlDatabase[shortURL] = {
     userID: req.session.user_id,
     url: longURL,
     date: new Date(),
     ipOfVisitors: []
   };
-  console.log(urlDatabase[shortURL])
-  res.redirect(`/urls/${shortURL}`);
+  return res.redirect(`/urls/${shortURL}`);
 });
+
+/**
+ * ------------------------------------------------------------------------
+ * EDIT SHORT URL PAGE
+ * ------------------------------------------------------------------------
+ */
+
+/**
+* Allows user to edit their short URLs and view the following analytics:
+* - date created
+* - times visited
+* - unique visits
+*
+* Renders HTML error if:
+* - short URL does not exist in database
+* - user is not logged in
+* - user is logged in but is not the user who created the short URL
+*/
 
 app.get('/urls/:id', (req, res) => {
   const { user_id } = req.session;
@@ -197,62 +315,99 @@ app.get('/urls/:id', (req, res) => {
     noUser: false,
     isNotAuthUser: false
   };
+
+  // Verifies if the short URL code exists in the database
   if (!urlDatabase.hasOwnProperty(id)) {
-    console.log('URL does not exist')
+    console.log('URL does not exist');
     templateVars.invalidURL = true;
-    return res.render('urls_show', templateVars)
+    return res.render('urls_show', templateVars);
+
+  // Checks if user is logged in
   } else if (!user_id) {
-    console.log('User not logged in')
+    console.log('User not logged in');
     templateVars.noUser = true;
-    console.log(templateVars)
-    return res.render('urls_show', templateVars)
+    return res.render('urls_show', templateVars);
+
+  // Checks if user accessing short URL page is the creator
   } else if (urlDatabase[id].userID !== user_id) {
-    console.log('User does not have access')
+    console.log('User does not have access');
     templateVars.isNotAuthUser = true;
-    return res.render('urls_show', templateVars)
+    return res.render('urls_show', templateVars);
   } else {
+    // Gets unique visitors but filtering duplicate IP address entries
+    const uniqueVisitors = (urlDatabase[id].ipOfVisitors).filter(getUnique);
+
     templateVars.longURL = urlDatabase[id].url;
     templateVars.user = users[user_id];
     templateVars.date = urlDatabase[id].date;
     templateVars.timesVisited = (urlDatabase[id].ipOfVisitors).length;
-    const uniqueVisitors = (urlDatabase[id].ipOfVisitors).filter(getUnique);
     templateVars.uniqueVisits = uniqueVisitors.length;
   }
   return res.render('urls_show', templateVars);
 });
 
+// POST to update short URL
 app.post('/urls/:id', (req, res) => {
   const { user_id } = req.session;
   const { id } = req.params;
   const { longURL } = req.body;
+
+  // Checks for valid URL format
   if (!validateWebsite(longURL)) {
-    return res.status(400).send('Please enter a valid URL')
-  };
+    return res.status(400).send(`<h1>Please enter a valid URL</h1><a href="/urls/${id}">Go back</a>`);
+  }
+
+  // Updates urlDatabase with new values
   urlDatabase[id] = {
     userID: user_id,
     url:longURL,
     date: urlDatabase[id].date,
     ipOfVisitors: urlDatabase[id].ipOfVisitors
   };
-  res.redirect(`/urls/${id}`);
+  return res.redirect(`/urls/${id}`);
 });
+
+/**
+ * ------------------------------------------------------------------------
+ * REDIRECTION
+ * ------------------------------------------------------------------------
+ */
 
 app.get('/u/:shortURL', (req, res) => {
+  const { user_id } = req.session;
   const { shortURL } = req.params;
+
+  // Verifies if shortURL exists in database, otherwise redirect
+  if (!urlDatabase.hasOwnProperty(shortURL)) {
+    return res.send('<h1>short URL code does not exist</h1><a href="/">Go back</a>');
+  };
   const longURL = urlDatabase[shortURL].url;
+
+  // Logs IP address of user to track unique visits
   urlDatabase[shortURL].ipOfVisitors.push(req.ip);
-  res.redirect(longURL);
+  return res.redirect(longURL);
 });
+
+/**
+ * ------------------------------------------------------------------------
+ * DELETE SHORT URL
+ * ------------------------------------------------------------------------
+ */
 
 app.post('/urls/:id/delete', (req, res) => {
-  const shortURL = req.params.id;
-  delete urlDatabase[shortURL];
-  res.redirect('/urls');
-});
+  const { user_id } = req.session;
+  const { id } = req.params;
 
-function generateRandomStr() {
-  return Math.random().toString(36).substring(6);
-};
+  // Verify user and user's permissions
+  if (!user_id) {
+    return res.status(401).send('<h1>401: Unauthorised</h1>');
+  } else if (urlDatabase[id].userID !== user_id) {
+    return res.status(401).send('<h1>403: Forbidden</h1>');
+  } else {
+    delete urlDatabase[id];
+    return res.redirect('/urls');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
